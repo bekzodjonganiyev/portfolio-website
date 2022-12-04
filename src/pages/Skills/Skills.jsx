8b76b7b0303668104ccbc7b91838cc1465a95281
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2022-12-05 03:47:50 +0500

    wip

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -25,18 +25,10 @@ const Skills = () => {
           />
         </h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          voluptatibus eaque aliquam at quam quae reiciendis minima tenetur aut
-          dolor in commodi, officia quo quisquam vero ab soluta minus molestias.
+          HTML, CSS, JavaScript, React( Redux, Redux Toolkit, Hooks, Formik),
+          Vue, Rest API, Git
         </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, dicta
-          quasi explicabo recusandae cupiditate autem nobis officia ab rerum
-          blanditiis.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, fugit.
-        </p>
+       
       </div>
     </div>
   )
